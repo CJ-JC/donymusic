0dbac32bc0e94ba72d91044969eff49f2bbf51cb
CJ <cherley95@hotmail.fr>
2025-02-03 17:54:59 +0100

    fix chemin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -7,10 +8,22 @@ export default defineConfig({
     include: ["dompurify"],
   },
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
     extensions: [".js", ".jsx", ".json"],
   },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
   server: {
+    port: 3000,
     proxy: {
       "/api": {
         target: "https://donymusic-server.vercel.app",
